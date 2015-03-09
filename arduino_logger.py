c27410bb2f56d8bf19e14abb2ea1ba7ab41f6f17
Rai <raiabril@hotmail.com>
2015-03-09 11:56:51 +0100

    I

---
+++ arduino_logger.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*- 
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Sep 12 2010)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO "NOT" EDIT THIS FILE!
+###########################################################################
+
+import wx
+import os
+import glob
+import sys
+from numpy import arange, sin, pi
+import matplotlib
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+import time as date
+from scipy.signal import butter, lfilter, freqz
+
+# comment out the following to use wx rather than wxagg
+matplotlib.use('WXAgg')
+
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+from matplotlib.figure import Figure
+
+def openSerial(strPort, baudRate):
+    ser = serial.Serial(strPort, baudRate)
+    return ser
+    
+def closeSerial(ser):
+    ser.flush()
+    ser.close()
+    
+def butter_lowpass(cutoff, fs, order = 5):
+    nyq = 0.5*fs
+    normal_cutoff = cutoff/nyq
+    b, a = butter(order, normal_cutoff, btype = 'low', analog = False)
+    return b, a
+    
+def butter_lowpass_filter(data, cutoff, fs, order = 5):
+    b, a = butter_lowpass(cutoff, fs, order = order)
+    y = lfilter(b, a, data)
+    return y
+    
+def serial_ports():
+    """Lists serial ports
+
+    :raises EnvironmentError:
+        On unsupported or unknown platforms
+    :returns:
+        A list of available serial ports
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM' + str(i + 1) for i in range(256)]
+
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this is to exclude your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+    
+def retrieveSamples(ser, user_input, delay):
+    time = []
+    data1 = []
+    data2 = []
+    command = str(int(user_input)) + '/' + str(int(delay)) + '/'
+    ser.write(command)
+    for i in range(1, int(user_input)+1):
+        line = ser.readline()
+        print line
+        line = line.split()
+        timeNum = float(line[0])
+        data1Num = float(line[1])
+        data2Num = float(line[2])
+        time.append(timeNum)
+        data1.append(data1Num)
+        data2.append(data2Num)
+    data = [time, data1, data2]    
+    return data
+
+###########################################################################
+## Class MyFrame1
+###########################################################################
+class CanvasPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.figure = Figure()
+        self.axes = self.figure.add_subplot(111)
+        self.canvas = FigureCanvas(self, -1, self.figure)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
+        self.SetSizer(self.sizer)
+        self.Fit()
+
+    def draw(self, data):
+        self.axes.plot(data[0], data[1],'b.', label = 'data1')
+        self.axes.plot(data[0], data[2], 'k.', label = 'data2')
+        order = 6
+        fs = 20.0 # Sample rate in Hz
+        cutoff = 3.667  
+        data1_filtered = butter_lowpass_filter(data[1], cutoff, fs, order)
+        data2_filtered = butter_lowpass_filter(data[2], cutoff, fs, order)
+        self.axes.plot(data[0], data1_filtered, 'b-')
+        self.axes.plot(data[0], data2_filtered, 'k-')
+        self.axes.legend()
+        self.axes.set_title('Data')
+        self.axes.set_xlabel('Samples')
+        self.axes.set_ylabel('Value')
+        
+class MyFrame1 ( wx.Frame ):
+
+	def __init__( self, parent ):
+	        box = wx.SingleChoiceDialog(None, 'Available ports', 'Select port!', serial_ports() )
+                if box.ShowModal() == wx.ID_OK:
+                    self.port = box.GetStringSelection()
+                    
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 1000,350 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )	
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		
+		self.m_menubar1 = wx.MenuBar( 0 )
+		self.m_menu1 = wx.Menu()
+		self.m_menuItem1 = wx.MenuItem( self.m_menu1, wx.ID_ANY, u"New", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu1.AppendItem( self.m_menuItem1 )
+		
+		self.m_menuItem2 = wx.MenuItem( self.m_menu1, wx.ID_ANY, u"Open", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu1.AppendItem( self.m_menuItem2 )
+		
+		self.m_menuItem3 = wx.MenuItem( self.m_menu1, wx.ID_ANY, u"About", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu1.AppendItem( self.m_menuItem3 )
+		
+		self.m_menu1.AppendSeparator()
+		
+		self.m_menuItem4 = wx.MenuItem( self.m_menu1, wx.ID_EXIT, u"Quit", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu1.AppendItem( self.m_menuItem4 )
+		
+		self.m_menubar1.Append( self.m_menu1, u"File" ) 
+		
+		self.SetMenuBar( self.m_menubar1 )
+		
+		general = wx.BoxSizer( wx.HORIZONTAL )
+		
+		plot = wx.BoxSizer( wx.VERTICAL )
+		
+		self.panel_plot = CanvasPanel(self)
+		plot.Add( self.panel_plot, 1, wx.EXPAND |wx.ALL, 5 )
+		
+		general.Add( plot, 1, wx.EXPAND, 5 )
+		
+		bSizer5 = wx.BoxSizer( wx.VERTICAL )
+		
+		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Arduino Logger! by Rai", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText2.Wrap( -1 )
+		bSizer5.Add( self.m_staticText2, 0, wx.ALL, 5 )
+		
+		self.m_staticText41 = wx.StaticText( self, wx.ID_ANY, u"Fichero", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText41.Wrap( -1 )
+		bSizer5.Add( self.m_staticText41, 0, wx.ALL, 5 )
+		
+		self.m_dirPicker2 = wx.DirPickerCtrl( self, wx.ID_ANY, wx.EmptyString, u"Select a folder", wx.DefaultPosition, wx.DefaultSize, wx.DIRP_DEFAULT_STYLE )
+		bSizer5.Add( self.m_dirPicker2, 0, wx.ALL, 5 )
+		
+		self.m_staticText6 = wx.StaticText( self, wx.ID_ANY, u"Parametros", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText6.Wrap( -1 )
+		bSizer5.Add( self.m_staticText6, 0, wx.ALL, 5 )
+		
+		bSizer13 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		bSizer14 = wx.BoxSizer( wx.VERTICAL )
+		
+		self.m_staticText3 = wx.StaticText( self, wx.ID_ANY, u"Numero de muestras", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText3.Wrap( -1 )
+		bSizer14.Add( self.m_staticText3, 0, wx.ALL, 5 )
+		
+		self.m_staticText4 = wx.StaticText( self, wx.ID_ANY, u"Frecuencia", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText4.Wrap( -1 )
+		bSizer14.Add( self.m_staticText4, 0, wx.ALL, 5 )
+		
+		bSizer13.Add( bSizer14, 1, wx.EXPAND, 5 )
+		
+		bSizer16 = wx.BoxSizer( wx.VERTICAL )
+		
+		self.m_textCtrl1 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer16.Add( self.m_textCtrl1, 0, wx.ALL, 5 )
+		
+		self.m_textCtrl2 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer16.Add( self.m_textCtrl2, 0, wx.ALL, 5 )
+		
+		bSizer13.Add( bSizer16, 1, wx.EXPAND, 5 )
+		
+		bSizer5.Add( bSizer13, 1, wx.EXPAND, 5 )
+		
+		self.m_button1 = wx.Button( self, wx.ID_ANY, u"Go!", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer5.Add( self.m_button1, 0, wx.ALL, 5 )
+		
+		general.Add( bSizer5, 1, wx.EXPAND, 5 )
+		
+		self.SetSizer( general )
+		self.Layout()
+		
+		self.Centre( wx.BOTH )
+	
+		# Events.
+                self.Bind(wx.EVT_MENU, self.OnExit, self.m_menuItem4)
+                self.Bind(wx.EVT_BUTTON, self.OnGo, self.m_button1)
+                self.m_dirPicker2.Bind( wx.EVT_DIRPICKER_CHANGED, self.updateDir )
+	
+	def __del__( self ):
+		pass
+		
+        def OnExit(self,e):
+            self.Close(True)  # Close the frame.
+            
+        def OnGo(self,e):
+            samples = self.m_textCtrl1.GetValue()
+            frequency = self.m_textCtrl2.GetValue()
+            baudRate = 9600
+            ser = openSerial(self.port, baudRate)
+            
+            delay = 1000/float(frequency)
+            data = retrieveSamples(ser, samples, delay)
+            closeSerial(ser)
+            self.panel_plot.axes.cla()
+            self.panel_plot.draw(data)
+            self.panel_plot.canvas.draw()
+            self.panel_plot.canvas.Refresh()
+            self.Show()
+            
+        def updateDir(self, e):
+            newpath = self.m_dirPicker2.GetPath()
+            os.chdir(newpath)
+            print newpath
+
+	
+
+if __name__ == '__main__':
+	app = wx.PySimpleApp()	   
+	frame = MyFrame1(parent = None)
+	frame.Show()
+	app.MainLoop()
+	
+	
